2fb9d81649f7bfd0e64e0215de2e9fa168ab082d
seasons986 <annalin12345@gmail.com>
2023-08-19 02:01:17 -0400

    fixed calculations for moon phases and added top margin to show scale a bitttt more accurately

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   // scale function from -- https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
   function scale (number, inMin, inMax, outMin, outMax) {
-    return Math.floor((number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
   }
 
   // NASA: These eight phases are, in order, new Moon, waxing crescent, first quarter, waxing gibbous, full Moon, waning gibbous, third quarter and waning crescent.
@@ -38,18 +38,14 @@ window.addEventListener("load", () => {
         lastMoon = d;
       }
     });
-    console.log(closestMoon);
-    console.log(lastMoon);
-
-    const moonEmojiIndex = scale(Date.now(), Date.parse(moons[lastMoon]), Date.parse(moons[closestMoon]), 0, 8);
-
-    // moons[closestMoon]
-
+    let moonEmojiIndex = scale(Date.now(), Date.parse(moons[lastMoon]), Date.parse(moons[closestMoon]), 0, 8);
+    let top = moonEmojiIndex % 1;
+    moonEmojiIndex = Math.floor(moonEmojiIndex);
     const container = document.getElementById("container");
+    container.style.marginTop =  `${top*10}px`;
     for (let i = 0; i < 8; i++) {
       const div = document.createElement("div");
-      let count = (moonEmojiIndex<(4-i) ? 8-(moonEmojiIndex-(4-i)): (i<5) ? moonEmojiIndex-(4-i) : moonEmojiIndex+i);
-      console.log({moonEmojiIndex}, {count});
+      let count = (moonEmojiIndex<(4-i) ? 8-((moonEmojiIndex-(4-i))*(-1)) : (i<5) ? moonEmojiIndex-(4-i) : moonEmojiIndex+(i-4));
       div.innerHTML = moonEmojis[count];
       container.appendChild(div);
     }
